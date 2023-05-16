e1831b5e97748bbb3318e48b49a2c40ae4f4c8db
GuillaumeChk <guillaume.chekhab39@gmail.com>
2023-05-16 13:27:24 +0200

    attempt 2

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "./",
+	base: "/site-perso/",
 	plugins: [
 		vue(),
 		quasar({
